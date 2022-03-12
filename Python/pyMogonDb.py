00e50cda507e7a1e93a570d457529c4da6c51f22
lizexian <64367160@qq.com>
2022-03-12 23:10:59 +0800

    delete

--- Python/pyMogonDb.py
+++
@@ -1,7 +0,0 @@
-from cgi import test
-from http import client
-import pymongo
-client = pymongo.MongoClient('localhost', 27017)
-mydb = client['mydb']
-test = mydb['test']
-test.insert_one({'name': 'Jan', 'sex': '男', 'grade': 89})
--- Python/pyMysqlDb.py
+++
@@ -1,9 +0,0 @@
-from email import charset
-import pymysql
-conn = pymysql.connect(host='localhost', user='root',
-                       passwd='', db='mydb', port=3306, charset='utf8')
-
-cursor = conn.cursor()
-cursor.execute(
-    "insert into students (name,sex,grade) values(%s,%s,%s)", ('张三', '女', 87))
-conn.commit()
